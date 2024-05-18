d3c04c2e059280a95692fe073fa7e35e4a5e6dd5
Quikdagger <quikdagger@gmail.com>
2024-05-18 12:31:55 -0400

    setup stages

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Welcome to Dungeons and Data</h1>
+    </div>
+  );
+}
+
+export default App;
